cb6a4a52f2455e01d9b42f3cd011e7af8e32ac7a
leuvi <528046@qq.com>
2019-10-21 23:40:03 +0800

    '添加异步路由'

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import {
   BrowserRouter,
   HashRouter,
@@ -8,11 +8,15 @@ import {
 } from 'react-router-dom'
 import { hot } from 'react-hot-loader/root'
 
-import Home from '@views/home/index.jsx'
-import Product from '@views/product/index.jsx'
-import Solution from '@views/solution/index.jsx'
-import Support from '@views/support/index.jsx'
-import About from '@views/about/index.jsx'
+const Home = lazy(() => import('@views/home/index.jsx'))
+const Product = lazy(() => import('@views/product/index.jsx'))
+const Solution = lazy(() => import('@views/solution/index.jsx'))
+const Support = lazy(() => import('@views/support/index.jsx'))
+const About = lazy(() => import('@views/about/index.jsx'))
+// import Product from '@views/product/index.jsx'
+// import Solution from '@views/solution/index.jsx'
+// import Support from '@views/support/index.jsx'
+// import About from '@views/about/index.jsx'
 
 const Router = process.env.NODE_ENV == 'development' ? HashRouter : BrowserRouter
 
@@ -20,14 +24,16 @@ class App extends React.PureComponent {
   render() {
     return (
       <Router>
-        <Switch>
-          <Route exact path="/" component={Home}></Route>
-          <Route exact path="/product" component={Product}></Route>
-          <Route path="/product/:name" component={Product}></Route>
-          <Route path="/solution" component={Solution}></Route>
-          <Route path="/support" component={Support}></Route>
-          <Route path="/about" component={About}></Route>
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/product" component={Product}></Route>
+            <Route path="/product/:name" component={Product}></Route>
+            <Route path="/solution" component={Solution}></Route>
+            <Route path="/support" component={Support}></Route>
+            <Route path="/about" component={About}></Route>
+          </Switch>
+        </Suspense>
       </Router>
     )
   }
